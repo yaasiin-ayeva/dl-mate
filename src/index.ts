9bd5fb33f36abe8abf5d65fc01671bffd4b8fb96
Yaasiin <ayevayaasiin@gmail.com>
2024-11-26 16:48:09 +0000

    fix : types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-export * from './types';
+export {
+    TikTokDownloadResult,
+    YouTubeDownloadFormat,
+    LinkedInDownloadResult,
+    XDownloadResult,
+    LinkedInVideoData,
+    Platform,
+    DlMateConfig
+} from './types';
 export { default as DlMate } from './dlmate';
 export { validate as validateUrl } from './validator';
